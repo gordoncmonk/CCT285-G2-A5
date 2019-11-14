e3c531c114d78aa89a127d30bac5aa3152e0652b
Gordon Monk <gordoncmonk@gmail.com>
2019-11-14 01:13:02 -0500

    Stage 1 - First Pass
    Implementing a mockup of the Moonside apartment using basic shapes to stub in for future props

---
+++ CCT285-G2-A5 - Moonside/Assets/Scripts/QuitGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+	{
+		// Quits the player when the user hits escape
+		if (Input.GetKey("escape"))
+		{
+			Application.Quit();
+		}
+	}
+}
